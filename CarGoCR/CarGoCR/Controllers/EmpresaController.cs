74aa6fc19400cbd5d6e0b9d7a2e4a2903d7e83a8
kevinga28 <kevinga2806@gmail.com>
2026-05-29 19:53:08 -0600

    Diseño

---
+++ CarGoCR/CarGoCR/Controllers/EmpresaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarGoCR.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
