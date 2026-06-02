2b392861474c3ab8fe2861cb056e5b0e57c3520e
kevinga28 <kevinga2806@gmail.com>
2026-06-02 17:50:31 -0600

    Diseños

--- CarGoCR/CarGoCR/obj/Debug/net10.0/CarGoCR.AssemblyInfo.cs
+++ CarGoCR/CarGoCR/obj/Debug/net10.0/CarGoCR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarGoCR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a59015628e3850112e8bec23b2dd1983bfe9470")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1508aafddae41671c37dda2cca9f6228a40c1da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarGoCR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarGoCR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
